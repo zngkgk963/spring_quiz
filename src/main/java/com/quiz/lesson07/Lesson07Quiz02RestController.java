eeeaea4dd8972b42b83aad92905c19bcaedf5448
test <test@gamil.com>
2023-07-06 18:43:58 +0900

    no message

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -1,8 +1,11 @@
 package com.quiz.lesson07;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quiz.lesson07.dao.RecruitRepository;
@@ -19,5 +22,44 @@ public class Lesson07Quiz02RestController {
 	@GetMapping("/1")
 	public RecruitEntity quiz02_1() {
 		return recruitRepository.findById(8).orElse(null);
+	}
+	
+	@GetMapping("/2")
+	public List<RecruitEntity> quiz02_2(
+			@RequestParam("companyId") int companyId) {
+		
+		return recruitRepository.findByCompanyId(companyId);
+	}
+	
+	@GetMapping("/3")
+	public List<RecruitEntity> quiz02_3() {
+		
+		return recruitRepository.findByPositionAndType("웹 back-end 개발자", "정규직");
+	}
+	
+	@GetMapping("/4")
+	public List<RecruitEntity> quiz02_4() {
+		//
+		return recruitRepository.findByTypeOrSalaryGreaterThanEqual("정규직", 9000);
+	}
+	
+	@GetMapping("/5")
+	public List<RecruitEntity> quiz02_5() {
+		
+		return recruitRepository.findTop3ByTypeOrderBySalaryDesc("계약직");
+	}
+	
+	@GetMapping("/6")
+	public List<RecruitEntity> quiz02_6() {
+		// 성남시 분당구가 지역인 연봉 7000 이상 8500 이하인 공고를 조회하고 아래와 같이 출력하세요.
+		return recruitRepository.findByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
+	}
+	
+	@GetMapping("/7")
+	public List<RecruitEntity> quiz02_7() {
+		// 마감일이 2026-04-10 이후이고 연봉이 8100 이상인 정규직 공고를 연봉 내림차순으로 조회하세요.
+		return recruitRepository
+				.findByDeadlineAfterAndSalaryAndTypeOrderBySalaryDesc(
+						"2026-04-10", 8100, "정규직");
 	}
 }
--- src/main/java/com/quiz/lesson07/dao/RecruitRepository.java
+++ src/main/java/com/quiz/lesson07/dao/RecruitRepository.java
@@ -3,6 +3,8 @@ package com.quiz.lesson07.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.quiz.lesson07.entity.RecruitEntity;
@@ -10,5 +12,25 @@ import com.quiz.lesson07.entity.RecruitEntity;
 @Repository
 public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer>{
 	
-	public List<RecruitEntity> findById(int id);
+	// JPQL -> Entity에 조회하는 개념
+	//quiz02_2
+	public List<RecruitEntity> findByCompanyId(int companyId);
+	
+	//quiz02_3
+	public List<RecruitEntity> findByPositionAndType(String position, String type);
+	
+	public List<RecruitEntity> findByTypeOrSalaryGreaterThanEqual(String type, int salary);
+	
+	public List<RecruitEntity> findTop3ByTypeOrderBySalaryDesc(String type);
+	
+	public List<RecruitEntity> findByRegionAndSalaryBetween(String region, int startSalary, int endSalary);
+	
+	// quiz02_7		native query => DB에 직접 조회(Mysql 쿼리문)
+	@Query(value = "select * from recruit where deadline > :deadline and salary >= :salary and type = :type order by salary desc", nativeQuery = true)
+	public List<RecruitEntity> findByDeadlineAfterAndSalaryAndTypeOrderBySalaryDesc(
+			@Param("deadline") String deadline,
+			@Param("salary") int salary,
+			@Param("type") String type);
+			
+	
 }
